3ce06257237f89159f4dae8c5eb3dd38833578c1
Runae <stella_nemesis@live.com>
2016-05-17 15:00:35 +0400

    Lesson 2 part 3

--- Lesson 2/4.rb
+++ Lesson 2/4.rb
@@ -1,5 +1,6 @@
 letters = {}
+voc = ["a","e","o","i","u","y"]
 ("a".."z").each_with_index do |l, i|
-	letters[l]=i+1 if l=="a" || l=="e" || l=="o" || l=="i" || l=="u" || l=="y"
+	letters[l]=i+1 if voc.include?(l)
 end
 puts letters
